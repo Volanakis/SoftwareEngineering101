219f227b5b05bebbc20e3576443be0aeaff8484c
LemonidasKost <leonkost36@gmail.>
2026-09-09 15:41:58 +0300

    Addition of screening model +mods in the models/__init__ file

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,13 @@
 from app.models.program import Program, ProgramRole, ProgramState, RoleType
+from app.models.screening import Screening, ScreeningState
 from app.models.user import User
 
-__all__ = ["User", "Program", "ProgramRole", "ProgramState", "RoleType"]
+__all__ = [
+    "User",
+    "Program",
+    "ProgramRole",
+    "ProgramState",
+    "RoleType",
+    "Screening",
+    "ScreeningState",
+]
